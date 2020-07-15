4706b7b38af6835ea1da9ca1778694435fc9f1ca
William Baumgartner <baumgartner.william@gmail.com>
2020-07-15 17:01:04 +0200

    feat: debounced search

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import './App.scss';
 
 import React, { useCallback, useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
-import queryString from 'query-string';
 import { useHistory, useLocation } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
+import queryString from 'query-string';
 import Main from './container/Main/Main';
-import { mockData } from './services/BookApi/FetchBooks.service';
+import fetchBooksApi from './services/BookApi/FetchBooks.service';
 import dataTransform from './services/BookTransform/BookTransform.service';
+
+// so we delay a bit user input
+// todo: move to a hook
+const debounce = (func, wait) => {
+  let timeout;
+  return (...args) => {
+    const context = this;
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      timeout = null;
+      func.apply(context, args);
+    }, wait);
+  };
+};
+
+const DEFAULT_DEBOUNCE_VALUE = 1000;
+
+// todo: move to env config & file
+const fetchBooks = fetchBooksApi({
+  fetch: axios.post,
+  baseUrl: 'http://nyx.vima.ekt.gr:3000/api/books'
+});
 
 function App() {
   const location = useLocation();
@@ -21,25 +44,31 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(initialPage);
   const [search, setSearch] = useState(initialSearch);
+  const onSearchChange = searchValue => {
+    // reset page
+    setPage(1);
+    setSearch(searchValue);
+  };
 
   const mainProps = {
     onNext: () => setPage(page + 1),
     onPrevious: () => setPage(page - 1),
-    onSearchChange: setSearch,
+    onSearchChange: debounce(onSearchChange, DEFAULT_DEBOUNCE_VALUE),
     search,
     loading,
     books
   };
 
+  //todo: negative pages on user input
   useEffect(() => {
     history.push(`${path}?page=${page}&search=${search}`);
   }, [page, search]);
 
   useEffect(() => {
     setLoading(true);
-    Promise.resolve(mockData).then(response => {
+    fetchBooks({ page, search }).then(response => {
       setLoading(false);
-      setBooks(response.books.map(dataTransform));
+      setBooks(response.data.books.map(dataTransform));
     });
   }, [page, search]);
 
